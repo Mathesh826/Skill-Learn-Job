dd940197ad1433038b06f8a94ba76f218382ca17
Mathesh@826 <mathesh3606@gmail.com>
2026-03-01 14:44:42 -0800

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,12 +9,17 @@ import random, time
 import traceback
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True
+)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")   # ADD THIS
 
 
 mail = Mail(app)
